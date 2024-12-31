304da5125b2c5942a6aa7d409284cedfa89b08f7
burlin <oddwes3.0@gmail.com>
2024-12-30 20:54:35 -0800

    fix save_page endpoint

--- src/pages/api/save_page/[slug].tsx
+++ src/pages/api/save_page/[slug].tsx
@@ -1,10 +1,11 @@
 import { convertReactToHtml } from "@/app/utils/reactToHtml";
 
 export default async function handler(req, res) {
-  const { slug } = req.query
-
   try {
-    const directoryCid = await convertReactToHtml(slug)
+    const { slug } = req.query
+    const protocol = req.headers["x-forwarded-proto"] || (req.connection.encrypted ? "https" : "http");
+    const domain = `${protocol}://${req.headers.host}`;
+    const directoryCid = await convertReactToHtml(slug, `${domain}/preview/${slug}`)
     res.status(200).json({ directoryCid })
   } catch (error) {
     res.status(500).send('Error generating self-contained HTML:', error);
